de446cdd18f9c20477170baa50ff1a9fee418824
Robert Cram <developer@axonsoftware.nl>
2017-06-29 20:41:00 +0200

    Add waiting loop to After('@javascript, @poltergeist') hook.

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -3,6 +3,9 @@ Before('@javascript, @poltergeist') do
 end
 
 After('@javascript, @poltergeist') do
+  Timeout.timeout(Capybara.default_max_wait_time) do
+    loop until page.evaluate_script('window.jQuery ? 0 : jQuery.active').zero?
+  end
   Capybara.reset_sessions!
   Capybara.current_driver = :rack_test
 end
