9a53ad12da58d0667009ee0420d865a0228a8801
Robert Cram <memberships@xs4all.nl>
2017-08-03 18:00:25 +0200

    Allow variable number of users in seeding.

---
+++ spec/db/seed_development_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe 'load admin.email, admin.password, business categories, regions and kommuns from ENV in production' do
+
+  env_seed_users = 'SEED_USERS'
+
+  # seed with a minimum of 4 users to cover: admin, no application, single application, double application
+  seed_users = 4
+
+  before(:all) do
+    RSpec::Mocks.with_temporary_scope do
+      allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new('development'))
+      stub_const('ENV', {env_seed_users => seed_users.to_s})
+      SHFProject::Application.load_tasks
+      SHFProject::Application.load_seed
+    end
+  end
+
+  it "business categories are in the db" do
+    expect(BusinessCategory.all.size).to eq(11)
+  end
+
+  it "regions are in the db" do
+    expect(Region.all.size).to eq(23)
+  end
+
+  it "kommuns are in the db" do
+    expect(Kommun.all.size).to eq(290)
+  end
+
+  it "users are in the db" do
+    expect(User.all.size).to eq(seed_users)
+  end
+
+  it "adresses are in the db" do
+    expect(Address.all.size).to eq(seed_users-1)
+  end
+
+  it "companies are in the db" do
+    expect(Company.all.size).to eq(seed_users-1)
+  end
+
+  it "memberships applications are in the db" do
+    expect(MembershipApplication.all.size).to eq(seed_users-1)
+  end
+
+end
