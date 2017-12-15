625d2e96d667b12b18883de68b9141295ac9b16e
Robert Cram <memberships@xs4all.nl>
2017-12-15 12:42:33 +0100

    Fix migration.

--- db/migrate/20170919120008_move_visibility_to_addresses.rb
+++ db/migrate/20170919120008_move_visibility_to_addresses.rb
@@ -5,14 +5,14 @@ class MoveVisibilityToAddresses < ActiveRecord::Migration[5.1]
       direction.up do
         add_column :addresses, :visibility, :string, default: 'street_address'
 
-        Company.all.each do |company|
-          # At the time of this migration we have only allowed one address
-          address = company.addresses.first
-          if address
-            address.visibility = company.address_visibility
-            address.save!
-          end
-        end
+        # Company.all.each do |company|
+        #   # At the time of this migration we have only allowed one address
+        #   address = company.addresses.first
+        #   if address
+        #     address.visibility = company.address_visibility
+        #     address.save!
+        #   end
+        # end
 
         remove_column :companies, :address_visibility
       end
@@ -20,13 +20,13 @@ class MoveVisibilityToAddresses < ActiveRecord::Migration[5.1]
       direction.down do
         add_column :companies, :address_visibility, :string, default: 'street_address'
 
-        Company.all.each do |company|
-          address = company.addresses.first
-          if address
-            company.address_visibility = address.visibility
-            company.save!
-          end
-        end
+        # Company.all.each do |company|
+        #   address = company.addresses.first
+        #   if address
+        #     company.address_visibility = address.visibility
+        #     company.save!
+        #   end
+        # end
 
         remove_column :addresses, :visibility
       end
